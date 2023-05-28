3ddf941563a8888068173d8b6591e1b6e63319c9
Rodnie <77215716+RealRodnie@users.noreply.github.com>
2023-05-28 02:04:32 +0100

    Delete js directory

--- js/result.js
+++
@@ -1,8 +0,0 @@
-const paragraph = document.querySelector('#paragraph');
-
-const params = new URLSearchParams(window.location.search);
-
-params.forEach((value, key)=>{
-    paragraph.append(`${value}`);
-    paragraph.append(document.createElement('br'));
-});
